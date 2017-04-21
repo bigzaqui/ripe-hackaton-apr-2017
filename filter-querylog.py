2504b823589240e8a9c12f392c0e0e35da57f8d6
Nick "Darkfiberiru" Wolff <darkfiberiru@gmail.com>
2017-04-21 13:19:40 +0000

    Adding tool to turn dns querylog into cvs file for easy examination

---
+++ filter-querylog.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from helpers import findAsn
+import sys
+#simple tool to play with dns query logs reformating into simple cvs output
+#filter-querylog.py "url filter" "asn to match against"
+with open('/usr/local/etc/namedb/log/querylog' , 'r')  as f:
+  for line in f:
+    if sys.argv[1] in line:
+      request_from = line.split(' ')[6].split('#')[0] 
+      probe_idi = line.split(' ')[7].split('.')[0].replace('(','')
+      timestamp = line.split(' ')[7].split('.')[1] 
+      asn = findAsn(request_from) 
+      if asn == sys.argv[1]:
+         result = ':)'
+      else:
+         result = ':('
+      print "{0},{1},{2},{3},{4}".format(probe_id,timestamp,request_from,asn,result)
+
