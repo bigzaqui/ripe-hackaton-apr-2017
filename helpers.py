b7ee35790f5776be0dee4b5aefa6f4613561e546
Edward Zambrano <zambrano@spotify.com>
2017-04-21 11:52:40 +0200

    works

--- helpers.py
+++ helpers.py
@@ -18,23 +18,28 @@ def on_result_response(*args):
     Function that will be called every time we receive a new result.
     Args is a tuple, so you should use args[0] to access the real message.
     """
-    ip = None
-    ans = args[0]['result']
-    if 'answers' in ans:
-        hostname = ans['answers'].pop()['NAME']
-        exittext = "{} -> ".format(hostname)
-        with open('/usr/local/etc/namedb/log/querylog') as f:
-            allgoogle = True
-            counter = 0
-            for line in f:
-                if hostname in line:
-                    counter +=1
-                    ip = line.split(' ')[6].split('#')[0]
-                    if findAsn(ip) != '15169':
-                        allgoogle = False
-                        break
-            exittext += "google!" if allgoogle else ":("
-            exittext += ", {} ocurrences in the logs".format(counter)
-            print exittext
-    else:
-        print "{}, no answer".format(args[0]['prb_id'])
+    try:
+        ip = None
+        if 'result' not in args[0]:
+            return False
+        ans = args[0]['result']
+        if 'answers' in ans:
+            hostname = ans['answers'].pop()['NAME']
+            exittext = "{} -> ".format(hostname)
+            with open('/usr/local/etc/namedb/log/querylog') as f:
+                allgoogle = True
+                counter = 0
+                for line in f:
+                    if hostname in line:
+                        counter +=1
+                        ip = line.split(' ')[6].split('#')[0]
+                        if findAsn(ip) != '15169':
+                            allgoogle = False
+                            break
+                exittext += "google!" if allgoogle else ":("
+                exittext += ", {} ocurrences in the logs".format(counter)
+                print exittext
+        else:
+            print "{}, no answer".format(args[0]['prb_id'])
+    except:
+        print args[0]
